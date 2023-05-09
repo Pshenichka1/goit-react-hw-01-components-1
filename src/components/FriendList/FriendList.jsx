900ec223d0b268a3455839726ca04c2f9daf3a53
Pshenichka1 <pshenichka.olga@gmail.com>
2023-05-10 00:43:00 +0300

    components

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,16 @@
+export const FriendList = ({ friends }) => {
+    return (
+        <div className="friends">
+            <ul className="friend-list">
+                {friends.map(({ avatar, name, isOnline, id }) => (
+                    <li className="item" key={id}>
+                        <span className="status">{isOnline}</span>
+                        <img className="avatar" src={avatar} alt={name} width="48" />
+                        <p className="name">{name}</p>
+                    </li>
+                ))}
+
+            </ul>
+        </div>
+    );
+}
